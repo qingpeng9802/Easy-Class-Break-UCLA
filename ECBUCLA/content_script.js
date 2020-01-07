fcbc3c41396c748cfd87fcd69f4d726ff86b2976
Qingpeng Li <qingpeng9802@gmail.com>
2020-01-06 19:09:38 -0800

    fix bug

--- ECBUCLA/content_script.js
+++ ECBUCLA/content_script.js
@@ -448,8 +448,8 @@ let contentScript = function (isFirstTime) {
     // [     0,         1,        2,  3,         4,       5        6,            7,       8]
 
     //Test
-    console.log(boxClasses);
-    console.log(planClasses);
+    //console.log(boxClasses);
+    //console.log(planClasses);
     calMinDiffOfBoxClasses();
     //Test
     //console.log(boxClasses);
